66f1ecc867bb787abfcbb25bfd260831fbbadb7b
yuki.kobayashi <ykobayashi134@gmail.com>
2023-03-11 17:24:25 +0000

    update

--- scripts/schedule.sh
+++ scripts/schedule.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Schedule execution of many runs
 # Run from root folder with: bash scripts/schedule.sh
+python3 src/train.py data.num_workers=20 trainer.max_epochs=5 logger=csv
 
-python src/train.py trainer.max_epochs=5 logger=csv
-
-python src/train.py trainer.max_epochs=10 logger=csv
+python3 src/train.py data.num_workers=20 trainer.max_epochs=10 logger=mlflow
