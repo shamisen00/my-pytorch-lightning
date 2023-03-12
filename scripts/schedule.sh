8b571231266d0da2a28e9bb0191ee488b8db32f8
yuki.kobayashi <ykobayashi134@gmail.com>
2023-03-12 08:22:56 +0000

    update logging exsample

--- scripts/schedule.sh
+++ scripts/schedule.sh
@@ -3,4 +3,4 @@
 # Run from root folder with: bash scripts/schedule.sh
 python3 src/train.py data.num_workers=20 trainer.max_epochs=5 logger=csv
 
-python3 src/train.py data.num_workers=20 trainer.max_epochs=10 logger=mlflow
+python3 src/train.py data.num_workers=20 trainer.max_epochs=5 logger=many_loggers
