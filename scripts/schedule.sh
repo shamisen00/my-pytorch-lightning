345faf821691beaf04a332a634611059d1580ee0
yuki.kobayashi <ykobayashi134@gmail.com>
2023-03-13 14:03:54 +0000

    update

--- scripts/schedule.sh
+++ scripts/schedule.sh
@@ -3,4 +3,4 @@
 # Run from root folder with: bash scripts/schedule.sh
 python3 src/train.py data.num_workers=20 trainer.max_epochs=5 logger=csv
 
-python3 src/train.py data.num_workers=20 trainer.max_epochs=5 logger=many_loggers
+python3 src/train.py data.num_workers=20 trainer.max_epochs=5 debug=profiler
