80a25214ab86a459f7983a29516aa9aaf44fc17d
yuki.kobayashi <ykobayashi134@gmail.com>
2023-03-15 14:39:54 +0000

    update

--- src/train_cli.py
+++ src/train_cli.py
@@ -2,7 +2,11 @@ from pytorch_lightning.cli import LightningCLI
 
 
 def main():
-    LightningCLI()
+    # To enable variable interpolation, first install omegaconf:
+    LightningCLI(save_config_filename="run.yaml",
+                 save_config_multifile=True, save_config_overwrite=True,
+                 parser_kwargs={"parser_mode": "omegaconf",
+                                "fit": {"default_config_files": ["configs/config.yaml"]}})
 
 
 if __name__ == "__main__":
