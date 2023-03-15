40160db9ae9b4d8307dd795ca6b7709ff1cb0db7
yuki.kobayashi <ykobayashi134@gmail.com>
2023-03-15 13:36:58 +0000

    fix

--- scripts/schedule.sh
+++ scripts/schedule.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 # Schedule execution of many runs
 # Run from root folder with: bash scripts/schedule.sh
-python3 src/train.py data.num_workers=20 trainer.max_epochs=5 logger=csv
-
-python3 src/train.py data.num_workers=20 trainer.max_epochs=5 debug=profiler
-
-python3 src/train_cli.py fit --trainer.logger false --trainer.max_epochs 5
+python3 train_cli.py fit --config config1.yaml --trainer.logger false --trainer.max_epochs 1
