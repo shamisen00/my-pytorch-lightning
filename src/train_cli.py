ee27683c3aaa38bb233e77394ffc30e6ebbeff65
yuki.kobayashi <ykobayashi134@gmail.com>
2023-03-15 13:37:08 +0000

    fix

--- src/train_cli.py
+++ src/train_cli.py
@@ -1,8 +1,9 @@
-# %%
-import pytorch_lightning as pl
 from pytorch_lightning.cli import LightningCLI
 
-from pytorch_lightning.demos.boring_classes import DemoModel, BoringDataModule
 
-# %%
-cli = LightningCLI(DemoModel, BoringDataModule)
+def main():
+    LightningCLI()
+
+
+if __name__ == "__main__":
+    main()
