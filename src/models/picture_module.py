40192bb648abc5e8eac6aba8b5223799b7ba0435
Yuki Kobayashi <37636462+shamisen00@users.noreply.github.com>
2023-03-19 07:16:49 +0900

    update

---
+++ src/models/picture_module.py
@@ -0,0 +1,110 @@
+from typing import Any
+
+import torch
+from pytorch_lightning import LightningModule
+from torchmetrics import MeanMetric
+
+
+class PictureModule(LightningModule):
+    """Example of LightningModule for MNIST classification.
+
+    A LightningModule organizes your PyTorch code into 6 sections:
+        - Computations (init)
+        - Train loop (training_step)
+        - Validation loop (validation_step)
+        - Test loop (test_step)
+        - Prediction Loop (predict_step)
+        - Optimizers and LR Schedulers (configure_optimizers)
+
+    Docs:
+        https://pytorch-lightning.readthedocs.io/en/latest/common/lightning_module.html
+    """
+
+    def __init__(
+        self,
+        net: torch.nn.Module,
+        optimizer: torch.optim.Optimizer,
+        scheduler: torch.optim.lr_scheduler,
+    ):
+        super().__init__()
+
+        # this line allows to access init params with 'self.hparams' attribute
+        # also ensures init params will be stored in ckpt
+        self.save_hyperparameters(logger=False)
+
+        self.net = net
+
+        # loss function
+        self.criterion = torch.nn.MSELoss()
+
+        self.train_loss = MeanMetric()
+        self.val_loss = MeanMetric()
+        self.test_loss = MeanMetric()
+
+    def forward(self, x: torch.Tensor):
+        return self.net(x)
+    
+    def on_train_start(self):
+        # by default lightning executes validation step sanity checks before training starts,
+        # so it's worth to make sure validation metrics don't store results from these checks
+        self.val_loss.reset()
+
+    def model_step(self, batch: Any):
+        x, y = batch
+        y_hat = self.forward(x)
+        loss = self.criterion(y_hat, y)
+        return loss, y
+
+    def training_step(self, batch: Any, batch_idx: int):
+        loss, targets = self.model_step(batch)
+
+        # update and log metrics
+        self.train_loss(loss)
+        self.log("train/loss", self.train_loss, on_step=False, on_epoch=True, prog_bar=True)
+
+        # we can return here dict with any tensors
+        # and then read it in some callback or in `training_epoch_end()` below
+        # remember to always return loss from `training_step()` or backpropagation will fail!
+        return {"loss": loss, "targets": targets}
+
+    def validation_step(self, batch: Any, batch_idx: int):
+        loss, targets = self.model_step(batch)
+
+        # update and log metrics
+        self.val_loss(loss)
+        self.log("val/loss", self.val_loss, on_step=False, on_epoch=True, prog_bar=True)
+
+        return {"loss": loss, "targets": targets}
+
+    def test_step(self, batch: Any, batch_idx: int):
+        loss, targets = self.model_step(batch)
+
+        self.test_loss(loss)
+        self.log("test/loss", self.test_loss, on_step=False, on_epoch=True, prog_bar=True)
+
+        return {"loss": loss, "targets": targets}
+
+    def configure_optimizers(self):
+        """Choose what optimizers and learning-rate schedulers to use in your optimization.
+        Normally you'd need one. But in the case of GANs or similar you might have multiple.
+
+        Examples:
+            https://pytorch-lightning.readthedocs.io/en/latest/common/lightning_module.html#configure-optimizers
+        """
+        optimizer = self.hparams.optimizer(params=self.parameters())
+        if self.hparams.scheduler is not None:
+            scheduler = self.hparams.scheduler(optimizer=optimizer)
+            return {
+                "optimizer": optimizer,
+                "lr_scheduler": {
+                    "scheduler": scheduler,
+                    "monitor": "val/loss",
+                    "interval": "epoch",
+                    "frequency": 1,
+                },
+            }
+        return {"optimizer": optimizer},
+
+
+if __name__ == "__main__":
+    _ = MNISTLitModule(None, None, None)
