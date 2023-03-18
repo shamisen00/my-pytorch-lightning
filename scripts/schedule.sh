288f9c4146c43690422826c4d211ce20ef1826e3
Yuki Kobayashi <37636462+shamisen00@users.noreply.github.com>
2023-03-19 07:04:59 +0900

    update

--- scripts/schedule.sh
+++ scripts/schedule.sh
@@ -4,3 +4,5 @@
 python3 src/train.py data.num_workers=20 trainer.max_epochs=5 logger=csv
 
 python3 src/train.py data.num_workers=20 trainer.max_epochs=5 debug=profiler
+
+python3 src/train.py data.num_workers=2 trainer.max_epochs=2 logger=csv data=pictures model=pictures callbacks.early_stopping.monitor="val/loss" callbacks.model_checkpoint.monitor="val/loss"
