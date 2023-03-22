6c9abbdb92d7356f5f98e0b0a342d58f0c9bffe1
yuki.kobayashi <ykobayashi134@gmail.com>
2023-03-22 19:41:36 +0000

    update

--- scripts/schedule.sh
+++ scripts/schedule.sh
@@ -5,4 +5,4 @@ python3 src/train.py data.num_workers=20 trainer.max_epochs=5 logger=csv
 
 python3 src/train.py data.num_workers=20 trainer.max_epochs=5 debug=profiler
 
-python3 src/train.py data.num_workers=2 trainer.max_epochs=2 logger=csv data=pictures model=pictures callbacks.early_stopping.monitor="val/loss" callbacks.model_checkpoint.monitor="val/loss"
+python3 src/train.py data.num_workers=2 trainer.max_epochs=5 logger=mlflow data=pictures model=pictures callbacks.early_stopping.monitor="val/loss" callbacks.model_checkpoint.monitor="val/loss" trainer.accelerator=gpu trainer.devices=1
